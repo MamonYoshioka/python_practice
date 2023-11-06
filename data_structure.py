5711a596f5759bc9f4025836c7932cf70d120dfe
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-06 11:36:58 +0000

    リストの値渡しと参照渡しの学習

--- data_structure.py
+++ data_structure.py
@@ -67,3 +67,21 @@ print(name)
 
 to_split = name.split(' ')
 print(to_split)
+
+# リストのコピーについて => （値渡しと参照渡し）
+
+i = [1,2,3,4,5]
+j = i
+j[0] = 100
+print('j=', j)
+print('i=', i)
+
+x = [1,2,3,4,5]
+y = x.copy()
+y[0] = 100
+
+print('y=', y)
+print('x=', x)
+
+print(id(x))
+print(id(y))
