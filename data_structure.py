5812aaf768fccf431918c293cb587a83e0721af4
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-06 11:14:24 +0000

    メソッド学習

--- data_structure.py
+++ data_structure.py
@@ -10,3 +10,26 @@ print(list[1:3])
 n = [1,2,3,4,5,6,7,8,9,10]
 print(n)
 print(n[::2])
+
+s = ['a','b','c','d','e','f','g']
+print(s)
+
+s[0] = 'X'
+print(s)
+
+s[2:5] = ['C','D','E']
+print(s)
+
+
+# メソッド
+n = [1,2,3,4,5,6,7,8,9,10]
+print(n)
+
+n.append(100)
+print(n)
+
+n.insert(0, 200)
+print(n)
+
+n.pop()
+print(n)
