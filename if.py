2604d0068d004bb29f61587db6127b66a7a1e19b
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-10 14:51:17 +0000

    into not

--- if.py
+++ if.py
@@ -42,3 +42,69 @@ if a > 0:
     print("a is positive")
     if b > 0:
       print("b is positive")
+
+# 論理演算子
+a = 1
+b = 1
+
+a == b
+a != b
+# aがｂよりも小さい
+a < b
+# aがｂよりも大きい
+a > b
+# aがｂ以下
+a <= b
+# aがｂ以上
+a >= b
+
+# 論理演算子
+# aもｂも真であれば真
+if a > 0 and b > 0:
+    print("a and b is positive")
+
+# a または bが真であれば真
+a = -1
+b = 1
+
+if a > 0 or b > 0:
+    print("a or b is positive")
+
+# in notの使用方法
+y = [1,2,3]
+x = 1
+
+if x in y:
+    print("in")
+
+if 100 not in y:
+    print("not in")
+
+a = 1
+b = 2
+
+if not a==b:
+    print("Not equal")
+
+# 数値比較の場合は以下のような記述が推奨されている
+if a != b:
+    print("Not equal")
+
+
+
+
+# is_ok = True
+is_ok = 1
+
+if not is_ok:
+    print("No")
+else:
+    print("OK")
+    
+arr = [1,2,3,4]
+
+if arr:
+    print("Ok")
+else:
+    print("Ng")
+
