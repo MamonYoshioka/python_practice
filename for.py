9f2516064343363851caff977b43ee169c5301cf
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-11 10:02:03 +0000

    range関数

--- for.py
+++ for.py
@@ -6,4 +6,39 @@ some_list = [1,2,3,4,5]
 #     i += 1
 
 for i in some_list:
-  print(i)
+    print(i)
+
+
+for i in 'abcde':
+    print(i)
+
+for word in ['my','name','is','mike']:
+    if word == 'name':
+        continue
+    print(word)
+
+
+# for else
+for fruit in ['apple','banana','orange']:
+    if fruit == 'banana':
+        print('stop eating')
+        break
+    print(fruit)
+else:
+    print('I ate all!')
+
+# range関数
+
+# num_list = [0,1,2,3,4,5,6,7,8,9]
+
+# for i in num_list:
+#     print(i)
+
+for i in range(10):
+    print(i)
+
+for x in range(2, 10, 3):
+    print(f'x is {x}')
+
+for _ in range(5):
+    print('hello')
